d55ea54022bd8bbfbef8b10c65a08f1de17e134b
AlexisRsgl <alexis.rsgl@laposte.net>
2022-11-18 09:56:59 +0100

    first commit

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,28 +2,33 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Connexion</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Page de connexion</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      
+      <ion-item>
+        <ion-label position="floating">Email</ion-label>
+        <ion-input type="text" v-model="Email"></ion-input>
+        <ion-label position="floating">Mot de passe</ion-label>
+        <ion-input type="text" v-model="Password"></ion-input>
+      </ion-item>
+      <ion-button color="tertiary" @click="Connexion">Connexion</ion-button>
+      <ion-button color="success">Déjà inscrit ?</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {defineComponent} from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/vue';
 
-export default  defineComponent({
-  name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-});
+export default defineComponent({
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonItem, IonLabel, IonInput, IonButton},
+})
 </script>
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>Inscription</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
@@ -11,8 +11,20 @@
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+      <ion-item>
+        <ion-label position="floating">Prénom</ion-label>
+        <ion-input type="text" v-model="firstname"></ion-input>
+        <ion-label position="floating">Nom</ion-label>
+        <ion-input type="text" v-model="name"></ion-input>
+        <ion-label position="floating">Email</ion-label>
+        <ion-input type="email" v-model="email"></ion-input>
+        <ion-label position="floating">Mot de passe</ion-label>
+        <ion-input type="password" v-model="password"></ion-input>
+        <ion-label position="floating">Confirmation du mot de passe</ion-label>
+        <ion-input type="password" v-model="password_confirmation"></ion-input>
+      </ion-item>
+      <ion-button color="success" @click="addUser">S'inscrire</ion-button>
+
     </ion-content>
   </ion-page>
 </template>
@@ -20,10 +32,9 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default defineComponent({
   name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
 });
 </script>
